cffd11e28333a13ff50137c2df85cff3c74ebedf
Mostafa <mostafagafer@gmail.com>
2024-09-24 14:42:08 +0200

    add settings
    adjust CSRF_TRUSTED_ORIGINS

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ else:
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ecomman.up.railway.app']
 
-CSRF_TRUSTED_ORIGIN = ['https://ecomman.up.railway.app']
+CSRF_TRUSTED_ORIGINS  = ['https://ecomman.up.railway.app']
 
 # Application definition
 
